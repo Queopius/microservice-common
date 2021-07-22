c25a991c0336157306490cc2a9c34fd7c4de6c77
Queopius A. S <dev.queopius@gmail.com>
2021-07-22 16:27:02 +0200

    First commit package

---
+++ src/Services/Traits/ConsumeExternalService.php
@@ -0,0 +1,28 @@
+<?php 
+
+namespace Queopius\MicroserviceCommon\Services\Traits;
+
+use Illuminate\Support\Facades\Http;
+
+trait ConsumeExternalService
+{
+    public function headers(array $headers = [])
+    {
+        array_push($headers, [
+            'Accepte' => 'application/json',
+            'Autorization' => $this->token
+        ]);
+
+        return Http::withHeaders($headers);
+    }
+
+    public function request(
+        string $method,
+        string $endPoint,
+        array $formParams = [],
+        array $headers = []
+    ) {
+        return $this->headers($headers)
+                    ->$method($this->url . $endPoint, $formParams);
+    }
+}
